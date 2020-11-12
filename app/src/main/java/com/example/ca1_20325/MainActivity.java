aa87e3735649f10ec1582384eeff740246e2bc1e
Bruno Mendanha <bmendanha@hotmail.com>
2020-11-12 14:13:26 +0000

    Created the database for PRODUCTS

--- app/src/main/java/com/example/ca1_20325/MainActivity.java
+++ app/src/main/java/com/example/ca1_20325/MainActivity.java
@@ -1,3 +1,6 @@
+//CA1 12/11/2020 - Mobile App 2 - BSC30920
+// ID: 20325 - Bruno H.M. Mendanha
+
 package com.example.ca1_20325;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,12 +10,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.ca1_20325.dbHelper.ConnectionSQLite;
+
 public class MainActivity extends AppCompatActivity {
 private Button btnInsProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // calling the created SQLite connection
+        ConnectionSQLite connectionSQLite = ConnectionSQLite.getInstance(this);
 
         this.btnInsProduct = (Button) findViewById(R.id.btnInsProduct);
 
---
+++ app/src/main/java/com/example/ca1_20325/dbHelper/ConnectionSQLite.java
@@ -0,0 +1,52 @@
+package com.example.ca1_20325.dbHelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConnectionSQLite extends SQLiteOpenHelper {
+
+
+    //this is a standard constructor for the database
+    private static  ConnectionSQLite INSTANCIA_CONEXAO;
+    private static final int VERSAO_DB =1;
+    private static final String NOME_DB = "ca1_products_app";
+
+    public ConnectionSQLite(Context context) {
+        super(context, NOME_DB, null, VERSAO_DB);
+    }
+    // Enf of the constructor for the database
+
+
+    //When calling this method, a test will be done to see if my instance is already null.
+    // If it is null it will be instantaneous otherwise it will be returned to what already exists.
+    public static  ConnectionSQLite getInstance(Context context){
+        if (INSTANCIA_CONEXAO == null){
+            INSTANCIA_CONEXAO = new ConnectionSQLite(context);
+
+        }
+        return INSTANCIA_CONEXAO;
+        }
+
+    @Override
+
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    String sqlProductTable =
+        "CREATE TABLE IF NOT EXISTS product" +
+                "(" +
+                "id INTEGER PRIMARY KEY,"+
+                "name TEXT," +
+                "quantity INTEGER,"+
+                "price REAL" +
+                ")";
+    sqLiteDatabase.execSQL(sqlProductTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
